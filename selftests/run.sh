7efd81a82fcc3f90adbc9adb157ee8174bf95255
B2F <d4rb2f@gmail.com>
2015-01-08 20:20:17 +0100

    Reducing npm test maintenance, keeping most stable elements

--- selftests/run.sh
+++ selftests/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-succss add selftests/data-defaults.js --pages=defaults,home --captures=body,fakeSelector --viewports=default
+succss add selftests/data-defaults.js --pages=defaults,home --captures=body,homebody,fake --viewports=default
 succss check selftests/data-defaults.js
-succss add selftests/data.js --rmtree
-succss check selftests/data.js
-succss check selftests/data-diff.js
+succss add selftests/data.js --pages=advanced-selectors  --rmtree
+succss check selftests/data.js --pages=advanced-selectors
+succss check selftests/data-diff.js --pages=advanced-selectors
