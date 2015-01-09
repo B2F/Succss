b78a987f7e69f476c66944b402de151e4a96ca99
B2F <d4rb2f@gmail.com>
2015-01-09 21:23:06 +0100

    casper-bridge.js cosmetic changes

--- casper-bridge.js
+++ casper-bridge.js
@@ -1,42 +1,46 @@
 /**
- * @file
+ * @file casper-bridge.js
  *
+ * Bridge bewteen succss commandline, casperjs and succss.js
  *
  */
-var fs = require('fs');
-
-var Succss = {};
-var cliOptions = casper.cli.options;
-
-if (['add', 'check', 'help'].indexOf(cliOptions.do) == -1) {
-  throw new Error('succss {{add | check} FILE.js} | help }');
-}
-
-if (fs.exists(cliOptions.dataFile)) {
-  phantom.injectJs(cliOptions.dataFile);
-}
-else {
-  throw "[Succss] File " + cliOptions.dataFile + " not found. Please enter a valid relative path.";
-}
-
-Succss.allOptions = {
-  imgType:'png',
-  imgQuality:'80',
-  tmpDir:'.succss-tmp',
-  diff:true,
-  imagediff:true,
-  resemble:false,
-  diffQuality:'80',
-  tolerancePixels:'0',
-};
-for (var opt in Succss.options) {
-  Succss.allOptions[opt] = Succss.options[opt];
-}
-for (var opt in cliOptions) {
-  Succss.allOptions[opt] = cliOptions[opt];
-}
-
 try {
+
+  var fs = require('fs');
+
+  var Succss = {};
+  var colorizer = require('colorizer').create('Colorizer');
+  var utils = require('utils');
+  var cliOptions = casper.cli.options;
+
+  if (['add', 'check', 'help'].indexOf(cliOptions.do) == -1) {
+    throw new Error('succss {{add | check} FILE.js} | help }');
+  }
+
+  if (fs.exists(cliOptions.dataFile)) {
+    phantom.injectJs(cliOptions.dataFile);
+  }
+  else {
+    throw '[Succss] File "' + cliOptions.dataFile + '" not found. Please enter a valid relative path.';
+  }
+
+  Succss.allOptions = {
+    imgType:'png',
+    imgQuality:'80',
+    tmpDir:'.succss-tmp',
+    diff:true,
+    imagediff:true,
+    resemble:false,
+    diffQuality:'80',
+    tolerancePixels:'0',
+  };
+  for (var opt in Succss.options) {
+    Succss.allOptions[opt] = Succss.options[opt];
+  }
+  for (var opt in cliOptions) {
+    Succss.allOptions[opt] = cliOptions[opt];
+  }
+
   if (cliOptions.do == 'help') {
     console.log('Capture base screenshots of successful CSS designs.');
     console.log('succss add FILE.js [--sets=a[,b,c...]] [--sections=d[,e,f...]] [--rmtree]');
@@ -53,8 +57,6 @@ try {
 
     Succss.casper = new casper.constructor(Succss.allOptions);
     var mouse = require("mouse").create(Succss.casper);
-    var colorizer = require('colorizer').create('Colorizer');
-    var utils = require('utils');
 
     var succss = require('succss.js').Succss.call(Succss);
 
@@ -67,8 +69,8 @@ try {
   }
 }
 catch(e) {
-  console.log(e);
+  console.log(colorizer.colorize(e, 'COMMENT'));
   console.log('See succss.ifzenelse.net for more infos.');
-  throw 'Wrong succss arguments. Type succss help for help.';
+  console.log('Wrong succss arguments. Type succss help for help.');
   casper.exit();
 }
