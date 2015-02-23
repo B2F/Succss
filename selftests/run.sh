b11e0397d50e1df634d5b067b184eb515d8a20a2
B2F <d4rb2f@gmail.com>
2015-02-23 22:26:05 +0100

    Adds readability to selftests/run.sh output (set -v and comments)

--- selftests/run.sh
+++ selftests/run.sh
@@ -1,38 +1,41 @@
 #!/bin/sh
-echo
-echo "add with configuration default values and  multiple filters and captures fake optionnal"
+set -v
+
+date
+
+# add with configuration default values and  multiple filters and captures fake optionnal
 succss add selftests/data-defaults.js --pages=defaults,home --captures=body,homebody,fake --viewports=default
-echo
-echo "check with default values and no filters"
+
+# check with default values and no filters
 succss check selftests/data-defaults.js
-echo
-echo "single filter, custom configuration and rmtree option"
+
+# single filter, custom configuration and rmtree option
 succss add selftests/data.js --pages=advanced-selectors --rmtree
-echo
-echo "check with a filter, imagediff (default) and resemble (optional)"
+
+# check with a filter, imagediff (default) and resemble (optional)
 succss check selftests/data.js --pages=advanced-selectors --resemble
-echo
-echo "Writing small diff images with minimum width"
+
+# Writing small diff images with minimum width
 succss add selftests/data.js --pages=diffCanvas
 succss check selftests/data-diff.js --pages=diffCanvas
-echo
-echo with static images comparison
+
+# with static images comparison
 succss check selftests/data.js --pages=diffCanvas --checkDir=selftests/static-images
-echo
-echo phantom base, phantom matches, slimer diff
+
+# phantom base, phantom matches, slimer diff
 succss add selftests/data-diff.js --pages=installation
 succss check selftests/data-diff.js --pages=installation --diff=false --good
 succss check selftests/data-diff.js --pages=installation --engine=slimerjs
-echo
-echo slimer base, phantom diff, slimer matches
+
+# slimer base, phantom diff, slimer matches
 succss add selftests/data-diff.js --pages=installation --engine=slimerjs
 succss check selftests/data-diff.js --pages=installation
-succss check selftests/data-diff.js --pages=installation --engine=slimerjs --diff=false --good
-echo
-echo custom capture property
+succss check selftests/data-diff.js --pages=installation --engine=slimerjs --diff=false --good --verbose
+
+# custom capture property
 succss add selftests/data-web-examples.js --viewports=classic-wide --pages=capture-prop
 succss check selftests/data-web-examples.js --viewports=classic-wide --pages=capture-prop
-echo
-echo callback 'before' after a window.scrollTo, special slimerjs fix
+
+# callback 'before' after a window.scrollTo, special slimerjs '=,' fix
 succss add selftests/data-web-examples.js --viewports=classic-wide --pages=,special --captures=body --engine=slimerjs
-succss check selftests/data-web-examples.js --viewports=classic-wide --pages=,special --captures=body --engine=slimerjs
+succss check selftests/data-web-examples.js --viewports=classic-wide --pages=,special --captures=body --engine=slimerjs --verbose
