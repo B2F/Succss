53f588737f00aa0f2f09659e2fd92fd8b5b92572
B2F <d4rb2f@gmail.com>
2015-02-23 22:29:06 +0100

    Replace the main npm test script with test.sh wrapper counting and reporting total failures and successes

---
+++ selftests/test.sh
@@ -0,0 +1,10 @@
+# Log latest test:
+./selftests/run.sh 2>&1 | tee ./selftests/run.log
+# Trim shell colors:
+sed -ri 's/\x1B\[[^m]*m//g' ./selftests/run.log
+if grep -rni 'Tests failed' ./selftests/run.log; then
+  echo 'selftests suite failed, see above lines from ./selftests/run.log'
+else
+  echo 'selftests suite is successful'
+fi
+
